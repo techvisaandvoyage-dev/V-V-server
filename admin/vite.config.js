c0d6f3ec4bc590b31f9f8b999c2630f273ed1cc8
VisaAndVoyage <tech.visaandvoyage@gmail.com>
2026-05-25 18:08:57 +0530

    Update admin base path for subdirectory hosting

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/admin/",
   server: {
     port: 5177,
     strictPort: false,
